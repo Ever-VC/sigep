b809e77e411e20a7ea137cc2694ca2f444d57c35
Ever Vásquez <102596002+Ever-VC@users.noreply.github.com>
2025-06-25 10:14:48 -0600

    Revert "✨ feat: restablecimiento de contraseña con Livewire y envío por correo"

--- resources/views/principal.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('titulo')
-    Principal
-@endsection
-
-@section('contenido')
-Pagina Principal
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,7 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
-use App\Livewire\Auth\ForgotPassword;
-use App\Livewire\Auth\ResetPassword;
 
-Route::get('/reset-password/{token}', ResetPassword::class)->name('password.reset');
 Route::get('/', function () {
-    return view('principal');
-});
-
-
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store']);
-Route::get('/forgot-password', ForgotPassword::class)->name('password.request');
-Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-Route::get('/probar-correo', function () {
-    Mail::raw('Este es un correo de prueba desde Laravel.', function ($message) {
-        $message->to('tu_correo@gmail.com')
-                ->subject('Correo de prueba SIGEP');
-    });
-
-    return 'Correo enviado';
+    return view('welcome');
 });
